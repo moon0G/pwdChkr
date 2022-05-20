db4dcda869f4a3dcd4bee5abf94bdf4de3048ed6
smlunar <61670949+moon0G@users.noreply.github.com>
2022-05-20 21:12:21 +1000

    Update pwdChk.ts

--- src/static/pwdChk.ts
+++ src/static/pwdChk.ts
@@ -1,1 +1,20 @@
+class characteristics {
+    num!: boolean;
+    low!: boolean;
+    upr!: boolean;
+    spl!: boolean;
+}
 
+function getCharacteristics(pwd:any) {
+    let c = new characteristics();
+    for(let i:number = 0; i<pwd.length; i++) {
+        if(/\d/.test(pwd[i])) {
+            c.num = true;
+            continue;
+        }// implement tests for special char & lower and upper
+    }
+    return c;
+}
+
+var c = getCharacteristics("0");
+console.log(c.num)
