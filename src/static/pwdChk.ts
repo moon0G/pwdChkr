0f97c15123e5348f1fe253422aa6841cfb03bac1
smlunar <61670949+moon0G@users.noreply.github.com>
2022-05-20 22:34:03 +1000

    Update pwdChk.ts

--- src/static/pwdChk.ts
+++ src/static/pwdChk.ts
@@ -3,18 +3,60 @@ class characteristics {
     low!: boolean;
     upr!: boolean;
     spl!: boolean;
+	typs!: number;
+
+	contructor() {
+		this.num = false;
+		this.low = false;
+		this.upr = false;
+		this.spl = false;
+	}
 }
 
 function getCharacteristics(pwd:any) {
-    let c = new characteristics();
-    for(let i:number = 0; i<pwd.length; i++) {
-        if(/\d/.test(pwd[i])) {
-            c.num = true;
-            continue;
-        }// implement tests for special char & lower and upper
-    }
-    return c;
+	let c = new characteristics();
+	c.typs = 0;
+	    for(let i:number = 0; i<pwd.length; i++) {
+			if(c.num!=true) {
+				if(/\d/.test(pwd[i])) {
+					c.num = true;
+					c.typs += 10;
+					continue;
+				}
+			}
+			if(c.spl!=true) {
+				if(/\W|_/g.test(pwd[i]) || pwd[i]==='_') {
+					c.spl = true;
+					c.typs += 30;
+					continue;
+				}
+			}
+			if(c.upr!=true){
+				if(pwd[i].toUpperCase()===pwd[i]){
+					c.upr = true;
+					c.typs += 26;
+					continue;
+				}
+			}
+			if(c.low!=true){
+				if(pwd[i].toLowerCase()===pwd[i]){
+					c.low = true;
+					c.typs += 26;
+					continue;
+				}
+			}
+		}
+	return c;
 }
 
-var c = getCharacteristics("0");
-console.log(c.num)
+function maxCalculations(pwd:string, c:characteristics) {
+	return c.typs ** pwd.length;
+}
+
+function onXCpu() { // i7-5600k 2.6ghz does 2.6e+9 (2,600,000,000c)ycles/second or 2.6e+9 (2,600,000,000)hertz (hz)
+
+}
+
+var pwd: string = "";
+var c = getCharacteristics("H3llo!");
+var calcs: number = maxCalculations(pwd, c);
