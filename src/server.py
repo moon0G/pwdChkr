7b8792e6106af2ccfa9347570405ae48d0c47448
smlunar <61670949+moon0G@users.noreply.github.com>
2022-06-08 19:06:19 +1000

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,20 +1,29 @@
 from flask import *
+import requests as netReq
+import json as j
 
-app = Flask(__name__)
+app = Flask(
+        __name__,
+        template_folder="templates",
+        static_folder="static"
+)
 
-@app.route('/')
+@app.route("/home")
+@app.route("/index")
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/password')
+@app.route("/check")
 def password():
-    return render_template('testPwd.html')
+    return render_template("testPwd.html")
 
-
-@app.route('/ret', methods=['POST', 'GET']) # method for return data from the frontend to the backend
+@app.route("/ret", methods=["POST", "GET"]) # method for return data from the frontend to the backend
 def ret():
     data = request.get_data().decode()
     print(data)
-    return render_template_string('')
-    
-app.run(debug=True)
+    return render_template_string('a')
+
+app.run("10.49.229.120", 3000, debug=True)
+#
+#192.168.1.151
