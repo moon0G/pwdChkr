89e22eabd686a763a1f627b2be25dd2555b03954
Byron Fanti <bfant@mybce.catholic.edu.au>
2022-07-22 14:06:47 +1000

    added handler

--- databaseHandler/handler.py
+++ databaseHandler/handler.py
@@ -1,8 +1,54 @@
 import csv
-import db
+import sqlite3 as sql
+import os
 
-def dbcease():
-    pass
+class handler:
+    def __init__(self):    
+        if not os.path.exists("cpudata.db"):
+            open("../data/cpudata.db", 'w')
+            
+            self.conn = sql.connect("cpudata.db")
 
-def dbexist():
-    pass
+            self.conn.cursor().execute("""\
+                CREATE TABLE cpus(
+                    ID INTEGER PRIMARY KEY UNIQUE,
+                    CPU TEXT,
+                    CodeName TEXT,
+                    RevealDate TEXT,
+                    Clock TEXT,
+                    SocketArch TEXT,
+                    Process TEXT,
+                    Cache TEXT,
+                    Wattage TEXT,
+                    ReleaseDate TEXT
+                );
+            """)
+
+            with open("../data/cpudata.csv", 'r') as cf:
+                reader = csv.DictReader(cf)
+                query = "INSERT INTO cpus VALUES (?,?,?,?,?,?,?,?,?,?)"
+
+                for row in reader:
+                    self.conn.execute(query,(
+                                      row["ID"], row["CPU"],
+                                      row["CodeName"], row["RevealDate"],
+                                      row["Clock"], row["SocketArch"],
+                                      row["Process"], row["Cache"],
+                                      row["Wattage"], row["ReleaseDate"]
+                    ))
+                self.conn.commit()
+        else:
+            self.conn = sql.connect("cpudata.db")
+
+    def query(self, query):
+        if os.path.exists("cpudata.db"):
+            exe = "SELECT * FROM cpus WHERE CPU LIKE '%" + query + "%'"
+            res = self.conn.cursor().execute(exe).fetchall()
+            for row in res:
+                print(f"{row[1]}: {row[4]}")
+
+    
+
+
+
+
