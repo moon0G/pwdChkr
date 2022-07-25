b68e4ba7a32afb1684283c32b9d3ebd39e510933
Byron Fanti <bfant@mybce.catholic.edu.au>
2022-07-25 13:54:57 +1000

    added cpuinfo method

--- server.py
+++ server.py
@@ -4,39 +4,41 @@ import random
 from socket import gethostname, gethostbyname
 from databaseHandler import handler
 
-handle = handler.handler()
-cpus = handle.query("", None)
+handle = handler.handler() # create var handle; initialise handle as handler() class
+global cpus; cpus = handle.query("", None) # create global var cpus; query all the names of the cpus for the dropdown menu on the site.
 
 app = Flask(
         __name__,
         template_folder="templates",
         static_folder="static"
-)
+) # default flask class
 
 @app.route("/home")
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", cpus=cpus) # default site index
 
-@app.route("/randomWords")
+@app.route("/randomWords") # non user route for generating the words
 def randomWords():
-    words = ""
-    with open("data/words.json", 'r') as f:
+    words = "" # empty string words
+    with open("data/words.json", 'r') as f: # open words.json as f
         f = f.read()
-        data = j.loads(f)
+        data = j.loads(f) # load it as python"DICT" object
 
         for x in range(0, 4):
             y = random.randint(0, 2465)
             if data["data"][y] not in words:
-                words += data["data"][y] + ' '
+                words += data["data"][y] + ' ' # for different random words
                     
-    return render_template_string(f"{words[:-1]}")
+    return render_template_string(f"{words[:-1]}") # return it removing the final space
 
-@app.route("/ret", methods=["POST", "GET"]) # method for return data from the frontend to the backend
-def ret():
-    data = request.get_data().decode()
-    print(data)
-    return render_template_string('Success: 200')
+@app.route("/cpuinfo", methods=["POST", "GET"]) # route for the data on cpus
+def cpuinfo():
+    handle = handler.handler() # flask freaks out when handler is initialised in a different thread, so its initialised every time here.
+    req = request.args.get("cpu")
+    res = f"{handle.query(req, 0)}" # wrap the object in a string so flask can handle it
+    
+    return render_template_string(res.replace('(', '[').replace(')', ']')) # need to make the tuple safe for js consumption (replacing paranthesis with square brackets)
 
 app.run(gethostbyname(gethostname()), 3000, debug=True)
